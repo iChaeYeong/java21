f05d665cdc9388a47f444b44ebc83bd5a89d8441
이채영 <ichaeyeong@ichaeyeong-ui-MacBookPro.local>
2026-03-27 14:43:52 +0900

    :Hello Test Project

---
+++ src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+
+	public static void main(String[] args) {
+		String str = "문자";
+		int iNum = 3;
+		float fNum = (float) 3.253;
+		// TODO Auto-generated method stub
+		System.out.println("안녕 자바");
+		System.out.println("hello");
+		System.out.printf("문자 = %s\n정수 = %d\n실수 = %f",str,iNum,fNum);
+	
+	}
+}
